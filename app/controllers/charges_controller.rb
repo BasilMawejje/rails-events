6361c3a3c17b14e448e9eb9369c994412d9ed60b
BasilMawejje <basil.mawejje@gmail.com>
2019-10-15 23:24:11 +0300

    Service Object works as expected Redirect payments to root path TODO: Add tests for Service Object

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,15 @@
 class ChargesController < ApplicationController
-
-  rescue_from Stripe::CardError, with: :catch_exception
-
   def new
   end
 
   def create
-    # binding.pry
-    Services::Charges::SampleService.new(charge_params, current_user).call
-    redirect_to new_charge_path
+    PaymentService.new(charge_params, current_user).call
+    redirect_to root_path
   end
 
   private
 
   def charge_params
     params.permit(:stripeEmail, :stripeToken, :cart)
-  end
-
-  def catch_exception
-    flash[:error] = exception.message
   end
 end
---
+++ app/services/payment_service.rb
@@ -0,0 +1,39 @@
+class PaymentService < ApplicationService
+  DEFAULT_CURRENCY = 'usd'.freeze
+
+  def initialize(params, user)
+    @stripe_email = params[:stripeEmail]
+    @stripe_token = params[:stripeToken]
+    @cart = user.get_cart_events_with_qty
+    @user = user
+  end
+
+  def call
+    create_charge(create_customer)
+  end
+
+  private
+
+  attr_accessor :user, :stripe_email, :stripe_token, :cart
+
+  def create_customer
+    customer = Stripe::Customer.create(
+      email: stripe_email,
+      source: stripe_token
+    )
+    customer
+  end
+
+  def create_charge(customer)
+    Stripe::Charge.create(
+      customer: customer.id,
+      amount: cart_amount,
+      description: "What's happening Kampala",
+      currency: DEFAULT_CURRENCY
+    )
+  end
+
+  def cart_amount
+    user.cart_total_price.to_i
+  end
+end
