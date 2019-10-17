d770b975acbeab255ae422507dc446d4a3b13c3f
BasilMawejje <basil.mawejje@gmail.com>
2019-10-17 16:17:05 +0300

    Add worker rsepc tests

---
+++ spec/workers/application_job_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe ApplicationJob, type: :job do
+  it "queues jobs" do
+    ActiveJob::Base.queue_adapter = :test
+    expect do
+      ApplicationJob.perform_later
+    end.to have_enqueued_job
+  end
+end
---
+++ spec/workers/order_mailer_worker_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+require "sidekiq/testing"
+Sidekiq::Testing.fake!
+
+RSpec.describe OrderMailerWorker, type: :worker do
+  describe "send an email" do
+    let(:user) { FactoryBot.create(:user) }
+
+    before(:each) do
+      Sidekiq::Worker.clear_all
+    end
+
+    it "should respond to #perform" do
+      expect(OrderMailerWorker.new).to respond_to(:perform)
+    end
+
+    it "queues job" do
+    worker_options = {
+      user:    user
+    }
+
+    expect do
+      described_class.perform_async(worker_options)
+    end.to change(described_class.jobs, :size).from(0).to(1)
+  end
+  end
+end
