8ca946718c6e48c11f9a7e871b448baf34c1822e
BasilMawejje <basil.mawejje@gmail.com>
2019-11-07 17:09:57 +0300

    Make email sending functionality dynamic for current user

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,9 @@
 class OrderMailer < ApplicationMailer
   def order_details(user)
     @user = user
-    @cart = User.first.current_user_cart
-    @order = User.first.get_cart_events_with_qty
+    current_customer = User.all.select{ |customer| customer.email == user }[0]
+    @cart = current_customer.current_user_cart
+    @order = current_customer.get_cart_events_with_qty
 
     attachments["order-#{@user}-#{DateTime.now}.pdf"] = OrderPdf.new(@order, view_context).render
     mail(to: @user, subject: "You're ticket has been booked.")
