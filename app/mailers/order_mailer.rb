d14768d245bd31dc6768b129482965b9dd604cc6
BasilMawejje <basil.mawejje@gmail.com>
2019-11-07 14:46:34 +0300

    Able to send pdf attachment to email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,10 @@
 class OrderMailer < ApplicationMailer
   def order_details(user)
     @user = user
+    @cart = User.first.current_user_cart
+    @order = User.first.get_cart_events_with_qty
+
+    attachments["order-#{@user}-#{DateTime.now}.pdf"] = OrderPdf.new(@order, view_context).render
     mail(to: @user, subject: "You're ticket has been booked.")
   end
 end
