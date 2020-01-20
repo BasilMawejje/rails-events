fd347a5a8b271ac9384948142a16dbf233114e85
BasilMawejje <basil.mawejje@gmail.com>
2020-01-20 15:03:31 +0300

    Refactor popular categories logic in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,6 +6,6 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     category_events_count = -> (c) { EventCategory.all.sample(10).lazy.select{|event_category| event_category.events.count > 15 }.take(5).to_a }
-    @popular_categories = EventCategory.all.lazy.map(&category_events_count).take(4).to_a.uniq.flatten.sample(4).map(&:name)
+    @popular_categories = EventCategory.all.lazy.flat_map(&category_events_count).take(4).to_a.map(&:name)
   end
 end
